78e6e79b786139f869f3f860a861da781149729d
Dimitry Medvedev <dimitry-m@yandex.ru>
2019-04-14 12:38:47 +0300

    Модификация запуска тестов

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-rm -rf allure-ui-results/*
-npm test
+
+SPEC=$1
+
+rm -rf allure-ui-results/*[!.json]
+
+if [ "$SPEC" = "" ]
+then
+  npm test
+else
+  ./node_modules/.bin/wdio --spec "$SPEC"
+fi
 ./node_modules/.bin/allure generate allure-ui-results --clean --output allure-ui-report
